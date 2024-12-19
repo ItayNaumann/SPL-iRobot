0032605536413fee78838cfc4d4084a25b426d16
itaynau <itay.naumann@gmail.com>
2024-12-19 15:58:25 +0200

    Finished microservice

--- src/main/java/bgu/spl/mics/MicroService.java
+++ src/main/java/bgu/spl/mics/MicroService.java
@@ -1,5 +1,7 @@
 package bgu.spl.mics;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * The MicroService is an abstract class that any micro-service in the system
  * must extend. The abstract MicroService class is responsible to get and
@@ -7,14 +9,15 @@ package bgu.spl.mics;
  * <p>
  * Derived classes of MicroService should never directly touch the message-bus.
  * Instead, they have a set of internal protected wrapping methods (e.g.,
- * {@link #sendBroadcast(bgu.spl.mics.Broadcast)}, {@link #sendBroadcast(bgu.spl.mics.Broadcast)},
+ * {@link #sendBroadcast(bgu.spl.mics.Broadcast)},
+ * {@link #sendBroadcast(bgu.spl.mics.Broadcast)},
  * etc.) they can use. When subscribing to message-types,
  * the derived class also supplies a {@link Callback} that should be called when
  * a message of the subscribed type was taken from the micro-service
  * message-queue (see {@link MessageBus#register(bgu.spl.mics.MicroService)}
  * method). The abstract MicroService stores this callback together with the
  * type of the message is related to.
- * 
+ *
  * Only private fields and methods may be added to this class.
  * <p>
  */
@@ -22,6 +25,10 @@ public abstract class MicroService implements Runnable {
 
     private boolean terminated = false;
     private final String name;
+    private MessageBus bus;
+    private ConcurrentHashMap<String, Callback> events;
+
+    private ConcurrentHashMap<String, Callback> broadcasts;
 
     /**
      * @param name the micro-service name (used mainly for debugging purposes -
@@ -29,6 +36,8 @@ public abstract class MicroService implements Runnable {
      */
     public MicroService(String name) {
         this.name = name;
+        this.events = new ConcurrentHashMap<>();
+        this.broadcasts = new ConcurrentHashMap<>();
     }
 
     /**
@@ -44,6 +53,7 @@ public abstract class MicroService implements Runnable {
      * {@link Callback#call(java.lang.Object)} by calling
      * {@code callback.call(m)}.
      * <p>
+     *
      * @param <E>      The type of event to subscribe to.
      * @param <T>      The type of result expected for the subscribed event.
      * @param type     The {@link Class} representing the type of event to
@@ -53,7 +63,8 @@ public abstract class MicroService implements Runnable {
      *                 queue.
      */
     protected final <T, E extends Event<T>> void subscribeEvent(Class<E> type, Callback<E> callback) {
-        //TODO: implement this.
+        bus.subscribeEvent(type, this);
+        events.put(type.getName(), callback);
     }
 
     /**
@@ -69,6 +80,7 @@ public abstract class MicroService implements Runnable {
      * {@link Callback#call(java.lang.Object)} by calling
      * {@code callback.call(m)}.
      * <p>
+     *
      * @param <B>      The type of broadcast message to subscribe to
      * @param type     The {@link Class} representing the type of broadcast
      *                 message to subscribe to.
@@ -77,40 +89,47 @@ public abstract class MicroService implements Runnable {
      *                 queue.
      */
     protected final <B extends Broadcast> void subscribeBroadcast(Class<B> type, Callback<B> callback) {
-        //TODO: implement this.
+        bus.subscribeBroadcast(type, this);
+        broadcasts.put(type.getName(), callback);
     }
 
     /**
-     * Sends the event {@code e} using the message-bus and receive a {@link Future<T>}
-     * object that may be resolved to hold a result. This method must be Non-Blocking since
+     * Sends the event {@code e} using the message-bus and receive a
+     * {@link Future<T>}
+     * object that may be resolved to hold a result. This method must be
+     * Non-Blocking since
      * there may be events which do not require any response and resolving.
      * <p>
-     * @param <T>       The type of the expected result of the request
-     *                  {@code e}
-     * @param e         The event to send
-     * @return  		{@link Future<T>} object that may be resolved later by a different
-     *         			micro-service processing this event.
-     * 	       			null in case no micro-service has subscribed to {@code e.getClass()}.
+     *
+     * @param <T> The type of the expected result of the request
+     *            {@code e}
+     * @param e   The event to send
+     * @return {@link Future<T>} object that may be resolved later by a different
+     *         micro-service processing this event.
+     *         null in case no micro-service has subscribed to {@code e.getClass()}.
      */
     protected final <T> Future<T> sendEvent(Event<T> e) {
-        //TODO: implement this.
-        return null; //TODO: delete this line :)
+        return bus.sendEvent(e);
+
     }
 
     /**
-     * A Micro-Service calls this method in order to send the broadcast message {@code b} using the message-bus
+     * A Micro-Service calls this method in order to send the broadcast message
+     * {@code b} using the message-bus
      * to all the services subscribed to it.
      * <p>
+     *
      * @param b The broadcast message to send
      */
     protected final void sendBroadcast(Broadcast b) {
-        //TODO: implement this.
+        bus.sendBroadcast(b);
     }
 
     /**
      * Completes the received request {@code e} with the result {@code result}
      * using the message-bus.
      * <p>
+     *
      * @param <T>    The type of the expected result of the processed event
      *               {@code e}.
      * @param e      The event to complete.
@@ -118,7 +137,7 @@ public abstract class MicroService implements Runnable {
      *               {@code e}.
      */
     protected final <T> void complete(Event<T> e, T result) {
-        //TODO: implement this.
+        bus.complete(e, result);
     }
 
     /**
@@ -148,10 +167,28 @@ public abstract class MicroService implements Runnable {
      */
     @Override
     public final void run() {
+        bus.register(this);
         initialize();
         while (!terminated) {
-            System.out.println("NOT IMPLEMENTED!!!"); //TODO: you should delete this line :)
-        }
-    }
+            try {
+                Massage msg = bus.awaitMessage(this);
+                if (msg != null) {
+                    if (msg instanceof Event) {
 
+                        Callback tmp = events.get(msg.getClass().getName());
+                        tmp.call(msg);
+                    } else {
+                        Callback tmp = broadcasts.get(msg.getClass().getName());
+                        tmp.call(msg);
+
+                    }
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+        bus.unregister(this);
+    }
 }
