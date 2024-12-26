6680ce2ae9daa4646daf4a2058c7c2afa3e629e7
itaynau <itay.naumann@gmail.com>
2024-12-26 17:40:41 +0200

    Added PoseService

--- src/main/java/bgu/spl/mics/application/services/PoseService.java
+++ src/main/java/bgu/spl/mics/application/services/PoseService.java
@@ -1,12 +1,20 @@
 package bgu.spl.mics.application.services;
 
 import bgu.spl.mics.MicroService;
+import bgu.spl.mics.application.messages.PoseEvent;
+import bgu.spl.mics.application.messages.TickBroadcast;
+import bgu.spl.mics.application.objects.GPSIMU;
+import bgu.spl.mics.application.objects.Pose;
 
 /**
- * PoseService is responsible for maintaining the robot's current pose (position and orientation)
+ * PoseService is responsible for maintaining the robot's current pose (position
+ * and orientation)
  * and broadcasting PoseEvents at every tick.
  */
 public class PoseService extends MicroService {
+
+    GPSIMU gpsImu;
+    int time;
 
     /**
      * Constructor for PoseService.
@@ -15,15 +23,22 @@ public class PoseService extends MicroService {
      */
     public PoseService(GPSIMU gpsimu) {
         super("Change_This_Name");
-        // TODO Implement this
+        this.gpsImu = gpsimu;
+        time = 0;
     }
 
     /**
      * Initializes the PoseService.
-     * Subscribes to TickBroadcast and sends PoseEvents at every tick based on the current pose.
+     * Subscribes to TickBroadcast and sends PoseEvents at every tick based on the
+     * current pose.
      */
     @Override
     protected void initialize() {
-        // TODO Implement this
+        subscribeBroadcast(TickBroadcast.class, (TickBroadcast msg) -> {
+            time++;
+            gpsImu.setCurTick(time);
+            Pose pose = gpsImu.getPose();
+            sendEvent(new PoseEvent(pose));
+        });
     }
 }
