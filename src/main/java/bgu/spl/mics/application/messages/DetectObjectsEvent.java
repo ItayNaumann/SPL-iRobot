2f229503b2e79858c7bf0c52313d658aed2cd5a4
itaynau <itay.naumann@gmail.com>
2025-01-01 15:03:40 +0200

    Thread safer

--- src/main/java/bgu/spl/mics/application/messages/DetectObjectsEvent.java
+++ src/main/java/bgu/spl/mics/application/messages/DetectObjectsEvent.java
@@ -3,7 +3,7 @@ package bgu.spl.mics.application.messages;
 import bgu.spl.mics.Event;
 import bgu.spl.mics.application.objects.StampedDetectedObjects;
 
-public class DetectObjectsEvent implements Event<StampedDetectedObjects> {
+public class DetectObjectsEvent implements Event<Boolean> {
     private StampedDetectedObjects detectedObject;
 
     public DetectObjectsEvent(StampedDetectedObjects detectedObject) {
--- src/main/java/bgu/spl/mics/application/services/CameraService.java
+++ src/main/java/bgu/spl/mics/application/services/CameraService.java
@@ -1,18 +1,13 @@
 package bgu.spl.mics.application.services;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import bgu.spl.mics.Event;
 import bgu.spl.mics.Future;
 import bgu.spl.mics.MicroService;
 import bgu.spl.mics.application.messages.TickBroadcast;
 import bgu.spl.mics.application.objects.Camera;
-import bgu.spl.mics.application.objects.DetectedObject;
 import bgu.spl.mics.application.messages.DetectObjectsEvent;
 import bgu.spl.mics.application.objects.StampedDetectedObjects;
-
 
 /**
  * CameraService is responsible for processing data from the camera and
@@ -26,7 +21,7 @@ public class CameraService extends MicroService {
     final Camera camera;
     int time;
     private ConcurrentLinkedQueue<StampedDetectedObjects> detectedQ;
-
+    // private detectedQ;
 
     /**
      * Constructor for CameraService.
@@ -48,27 +43,32 @@ public class CameraService extends MicroService {
      */
     @Override
     protected void initialize() {
-        
+
         subscribeBroadcast(TickBroadcast.class, c -> {
             StampedDetectedObjects sdo = getObjByTime();
-            if (sdo != null){
+            if (sdo != null) {
                 detectedQ.add(sdo);
             }
-            if (!detectedQ.isEmpty() && detectedQ.peek().time() == time - camera.freq()){
-                Future<Boolean> b = sendEvent(new DetectObjectsEvent<>(detectedQ.remove()));
+            if (!detectedQ.isEmpty() && detectedQ.peek().time() == time - camera.freq()) { // T + F in p 14, im pretty
+                // sure
+                StampedDetectedObjects d;
+                synchronized (detectedQ) {
+                    d = detectedQ.remove();
+                }
+                // You can choose to do something with b
+                Future b = sendEvent(new DetectObjectsEvent(d));
             }
             time++;
         });
 
     }
-    private StampedDetectedObjects getObjByTime(){
-        for (StampedDetectedObjects o : camera.detectedObjectsList()){
+
+    private StampedDetectedObjects getObjByTime() {
+        for (StampedDetectedObjects o : camera.detectedObjectsList()) {
             if (o.time() == time)
                 return o;
         }
         return null;
     }
 
-    
-    
 }
