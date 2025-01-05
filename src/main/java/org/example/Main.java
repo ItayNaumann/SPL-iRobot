5ab85fc48a862a8f058523e6b76fbec07e8595cb
Omer <omerlevitin2007@gmail.com>
2025-01-05 17:39:31 +0200

    Added JSON parsing for Pose_data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import bgu.spl.mics.application.objects.Pose;
 import bgu.spl.mics.application.objects.StampedDetectedObjects;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -11,11 +12,19 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        // Read from JSON
         Gson gson = new Gson();
         try (FileReader reader = new FileReader("camera_data.json")){
             Type SDOList = new TypeToken<List<StampedDetectedObjects>>(){}.getType();
             List<StampedDetectedObjects> cameraDataList = gson.fromJson(reader, SDOList);
 
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        try (FileReader reader = new FileReader("pose_data.json")){
+            Type POSEList = new TypeToken<List<Pose>>(){}.getType();
+            List<Pose> poseDataList = gson.fromJson(reader, POSEList);
         }
         catch (IOException e){
             e.printStackTrace();
