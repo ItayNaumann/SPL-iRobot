f2b742c6f8a103ae97040560e1146ab549171a8b
itaynau <itay.naumann@gmail.com>
2025-01-09 17:02:01 +0200

    Added another fusionSlam test

--- src/test/java/bgu/spl/mics/FusionSlamTest.java
+++ src/test/java/bgu/spl/mics/FusionSlamTest.java
@@ -13,7 +13,6 @@ class FusionSlamTest {
     @Test
     void calcLandMarkTest1() {
 
-
         Pose pose1 = new Pose(1.0, 2.5, 0.0, 2);
         Pose pose2 = new Pose(1.0, 3.5, 45.0, 4);
         Pose pose3 = new Pose(1.5, 5.0, 90, 6);
@@ -42,7 +41,6 @@ class FusionSlamTest {
         points1.getLast().rotate(-pose1.yaw);
         TrackedObject to1 = new TrackedObject("1", 2, "wall1", points1);
 
-
         CloudPoint p4 = new CloudPoint(1.5, 4.0);
         CloudPoint p5 = new CloudPoint(1.25, 4.25);
         CloudPoint p6 = new CloudPoint(1.75, 3.75);
@@ -58,7 +56,6 @@ class FusionSlamTest {
         points2.addLast(new CloudPoint(p4.x() - pose2.x, p4.y() - pose2.y));
         points2.getLast().rotate(-pose2.yaw);
         TrackedObject to2 = new TrackedObject("2", 4, "toy", points2);
-
 
         CloudPoint p7 = new CloudPoint(2.0, 6.0);
         CloudPoint p8 = new CloudPoint(1.5, 6.0);
@@ -94,7 +91,6 @@ class FusionSlamTest {
     @Test
     void calcLandMarkTest2() {
 
-
         Pose pose1 = new Pose(1.0, 2.5, 0.0, 2);
         Pose pose2 = new Pose(1.0, 3.5, 135.0, 4);
         Pose pose3 = new Pose(1.5, 5.0, 45, 6);
@@ -125,14 +121,12 @@ class FusionSlamTest {
         points1.getLast().rotate(-pose1.yaw);
         TrackedObject to1 = new TrackedObject("1", 2, "wall1", points1);
 
-
         CloudPoint p4 = new CloudPoint(0.65, 3.65);
         CloudPoint p5 = new CloudPoint(0.75, 3.75);
         CloudPoint p6 = new CloudPoint(0.85, 3.85);
         list2.add(p4);
         list2.add(p5);
         list2.add(p6);
-
 
         LinkedList<CloudPoint> points2 = new LinkedList<>();
         points2.addLast(new CloudPoint(p6.x() - pose2.x, p6.y() - pose2.y));
@@ -142,7 +136,6 @@ class FusionSlamTest {
         points2.addLast(new CloudPoint(p4.x() - pose2.x, p4.y() - pose2.y));
         points2.getLast().rotate(-pose2.yaw);
         TrackedObject to2 = new TrackedObject("2", 4, "toy", points2);
-
 
         CloudPoint p7 = new CloudPoint(2.0, 6.0);
         CloudPoint p8 = new CloudPoint(2.0, 5.75);
@@ -167,6 +160,14 @@ class FusionSlamTest {
         list4.add(p11);
         list4.add(p12);
 
+        LinkedList<CloudPoint> points5 = new LinkedList<>();
+
+        TrackedObject to5 = new TrackedObject("4", 6, "wall3", points3);
+
+        CloudPoint p13 = new CloudPoint(-1.5, 6.0);
+        CloudPoint p14 = new CloudPoint(-1.75, 6.0);
+        CloudPoint p15 = new CloudPoint(-2.0, 6.0);
+
         LinkedList<CloudPoint> points4 = new LinkedList<>();
         points4.addLast(new CloudPoint(p10.x() - pose3.x, p10.y() - pose3.y));
         points4.getLast().rotate(-pose3.yaw);
@@ -182,7 +183,6 @@ class FusionSlamTest {
         fs.addPose(pose2);
         fs.addPose(pose3);
 
-
         LandMark l1 = new LandMark(to1.getID(), to1.getDescription(), list1);
         LandMark l2 = new LandMark(to2.getID(), to2.getDescription(), list2);
         LandMark l3 = new LandMark(to3.getID(), to3.getDescription(), list3);
@@ -193,4 +193,64 @@ class FusionSlamTest {
         assertTrue(fs.calcLandMark(to3).equals(l3) & l3.getCoordinates().equals(list3));
         assertTrue(fs.calcLandMark(to4).equals(l4) & l4.getCoordinates().equals(list4));
     }
+
+    @Test
+    void calcLandMarkTest3() {
+
+        Pose pose1 = new Pose(1.0, 2.5, 0.0, 2);
+        Pose pose2 = new Pose(1.0, 3.5, 0.0, 4);
+
+        List<CloudPoint> list1 = new ArrayList<>();
+        List<CloudPoint> list2 = new ArrayList<>();
+
+        CloudPoint p1 = new CloudPoint(2.0, 2.0);
+        CloudPoint p2 = new CloudPoint(2.0, 2.5);
+        CloudPoint p3 = new CloudPoint(2.0, 3.0);
+        list1.add(p1);
+        list1.add(p2);
+        list1.add(p3);
+
+        LinkedList<CloudPoint> points1 = new LinkedList<>();
+        points1.addLast(new CloudPoint(p3.x() - pose1.x, p3.y() - pose1.y));
+        points1.getLast().rotate(-pose1.yaw);
+        points1.addLast(new CloudPoint(p2.x() - pose1.x, p2.y() - pose1.y));
+        points1.getLast().rotate(-pose1.yaw);
+        points1.addLast(new CloudPoint(p1.x() - pose1.x, p1.y() - pose1.y));
+        points1.getLast().rotate(-pose1.yaw);
+        TrackedObject to1 = new TrackedObject("1", 2, "wall1", points1);
+
+        CloudPoint p4 = new CloudPoint(2.0, 2.0);
+        CloudPoint p5 = new CloudPoint(2.0, 2.5);
+        CloudPoint p6 = new CloudPoint(2.0, 3.0);
+        list2.add(p4);
+        list2.add(p5);
+        list2.add(p6);
+
+        LinkedList<CloudPoint> points2 = new LinkedList<>();
+        CloudPoint prevFirst = points1.get(0);
+        CloudPoint curFirst = new CloudPoint(p6.x() - pose2.x, (p6.y() - pose2.y));
+        curFirst.rotate(-pose2.yaw);
+        points2.addLast(new CloudPoint((curFirst.x() + prevFirst.x()) / 2, (curFirst.y() + prevFirst.y()) / 2));
+        CloudPoint prevSecond = points1.get(1);
+        CloudPoint curSecond = new CloudPoint(p5.x() - pose2.x, (p5.y() - pose2.y));
+        curSecond.rotate(-pose2.yaw);
+        points2.addLast(new CloudPoint((curSecond.x() + prevSecond.x()) / 2, (curSecond.y() + prevSecond.y()) / 2));
+        CloudPoint prevThird = points1.get(2);
+        CloudPoint curThird = new CloudPoint(p4.x() - pose2.x, (p4.y() - pose2.y));
+        curThird.rotate(-pose2.yaw);
+        points2.addLast(new CloudPoint((curThird.x() + prevThird.x()) / 2, (curThird.y() + prevThird.y()) / 2));
+        TrackedObject to2 = new TrackedObject("1", 4, "wall1", points2);
+
+        FusionSlam fs = FusionSlam.getInstance();
+
+        fs.addPose(pose1);
+        fs.addPose(pose2);
+
+        LandMark l1 = new LandMark(to1.getID(), to1.getDescription(), list1);
+        LandMark l2 = new LandMark(to2.getID(), to2.getDescription(), list2);
+
+        assertTrue(fs.calcLandMark(to1).equals(l1));
+        assertTrue(fs.calcLandMark(to2).equals(l2));
+    }
+
 }
